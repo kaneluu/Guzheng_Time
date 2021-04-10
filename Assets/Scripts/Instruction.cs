c4442373e811c0596e7085becd8d7f08e7b5f91c
Quyen Luu <qluu8268@sdsu.edu>
2021-04-09 21:24:33 -0700

    Adding Scenes

---
+++ Assets/Scripts/Instruction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Instruction : MonoBehaviour
+{
+    public Button TutorialButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        TutorialButton = GameObject.Find("Tutorial Button").
+            GetComponent<Button>();
+        TutorialButton.onClick.AddListener(() => CharacterScene(2));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void CharacterScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
---
+++ Assets/Scripts/MainSceneButtons.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainSceneButtons : MonoBehaviour
+{
+    public Button PlayButton;
+    public Button QuitButton;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        PlayButton = GameObject.Find("Play Button").
+            GetComponent<Button>();
+        PlayButton.onClick.AddListener(() => CharacterScene(1));
+        QuitButton = GameObject.Find("Quit Button").
+            GetComponent<Button>();
+        QuitButton.onClick.AddListener(() => Quit());
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void CharacterScene(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+}
