f180c0087a704b640da35b1bdd6bd2ff2402ca72
Quyen Luu <qluu8268@sdsu.edu>
2021-04-12 14:42:36 -0700

    Adding buttons and test

---
+++ Assets/Notes.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Notes : MonoBehaviour
+{
+    //The speed of the notes based on the BPM of the song
+
+    Rigidbody2D rBody;
+    public float speed;
+
+    void Awake()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rBody.velocity = new Vector2(0, -speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Prefabs/PressedButtons.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressedButtons : MonoBehaviour
+{
+    SpriteRenderer sRen;
+    public KeyCode Key;
+    Color old;
+
+    //boolean variable to indicate the trigger
+    bool pressed = false;
+
+    //a GameObject variable for our note
+    GameObject note;
+
+    void Awake()
+    {
+        sRen = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+        old = sRen.color;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(Key))
+            StartCoroutine(Press());
+
+        if(Input.GetKeyDown(Key) && pressed)
+        {
+            Destroy(note);
+            StartCoroutine(Press());
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        //if trigger, pressed is true
+        pressed = true;
+        if (collision.gameObject.tag == "Note")
+        {
+            note = collision.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        pressed = false;
+    }
+
+    IEnumerator Press()
+    {
+        sRen.color = new Color(0, 1, 1);
+        yield return new WaitForSeconds(0.05f);
+        sRen.color = old;
+    }
+}
