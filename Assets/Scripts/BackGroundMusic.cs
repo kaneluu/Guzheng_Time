1fe864dbb87c96fb3c8225e65849d4e5935d8d2a
Quyen Luu <qluu8268@sdsu.edu>
2021-04-12 19:44:04 -0700

    Adding level 1's aspects

---
+++ Assets/Scripts/BackGroundMusic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundMusic : MonoBehaviour
+{
+
+    //for background music
+    private static BackGroundMusic instance = null;
+    public static BackGroundMusic Instance
+    {
+        get { return instance; }
+    }
+
+    void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StopBackgroundMusic.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopBackgroundMusic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BackGroundMusic.Instance.gameObject.GetComponent<AudioSource>().Pause();
+    }
+}
