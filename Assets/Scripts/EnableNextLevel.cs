456d977daac433ed26d21e130dd8cf121fc4439f
Quyen Luu <qluu8268@sdsu.edu>
2021-04-15 15:09:21 -0700

    Adding next level button

--- Assets/Scripts/EnableNextLevel.cs
+++ Assets/Scripts/EnableNextLevel.cs
@@ -1,18 +1,51 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class EnableNextLevel : MonoBehaviour
 {
+    public Button nextLevel;
+    public AudioSource music;
+    private float duration;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        nextLevel = GameObject.Find("Next Level").
+       GetComponent<Button>();
+        nextLevel.interactable = false;
+        nextLevel.GetComponent<Image>().color = new Color(1f, 1f, 1f);
+
+        //When the music still plays, the button stays unclickable
+        music = GetComponent<AudioSource>();
+        duration = music.clip.length;
+        StartCoroutine(DelayTime());
+
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator DelayTime()
     {
-        
+        music.Play();
+        yield return new WaitForSeconds(duration);
+        nextLevel.interactable = true;
+        // Create a temporary reference to the current scene.
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // Retrieve the name of this scene.
+        string sceneName = currentScene.name;
+
+        if(sceneName == "Level1")
+            nextLevel.onClick.AddListener(() => CharacterScene("Level2"));
+        else if(sceneName == "Level2")
+            nextLevel.onClick.AddListener(() => CharacterScene("Level3"));
+
+
+
+    }
+    public void CharacterScene(string level)
+    {
+        SceneManager.LoadScene(level);
     }
 }
